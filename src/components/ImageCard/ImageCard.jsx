88b706198451c1bab0e9f83820801e3caba49dce
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-09-27 10:12:18 +0200

    Update ImageCard.jsx

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,15 +3,10 @@ import styles from './ImageCard.module.css';
 
 const ImageCard = ({ image, onClick }) => {
   return (
-    <div className={styles.ImageCard}>
-      <img
-        src={image.urls.small}
-        alt={image.description || 'Image'}
-        onClick={() => onClick(image)}
-      />
+    <div className={styles.card} onClick={onClick}>
+      <img src={image.urls.small} alt={image.description} className={styles.image} />
     </div>
   );
 };
 
 export default ImageCard;
-
