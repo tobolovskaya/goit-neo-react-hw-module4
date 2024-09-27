ceaa76c9b04c7ab3557306eedef71ba4653218aa
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-09-27 10:13:23 +0200

    Update ErrorMessage.jsx

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './ErrorMessage.module.css';
 
-const ErrorMessage = ({ message }) => (
-  <div className={styles.error}>
-    <p>{message}</p>
-  </div>
-);
+const ErrorMessage = ({ message }) => {
+  return (
+    <div className={styles.errorMessage}>
+      <p>{message}</p>
+    </div>
+  );
+};
 
 export default ErrorMessage;
