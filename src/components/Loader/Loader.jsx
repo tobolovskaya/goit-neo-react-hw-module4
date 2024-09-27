fc90a806fbb94113cd50141706ead4cc2a9e8e22
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-09-27 10:12:40 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { TailSpin } from 'react-loader-spinner'; 
+import { TailSpin } from 'react-loader-spinner';
 import styles from './Loader.module.css';
 
 const Loader = () => {
   return (
-    <div className={styles.Loader}>
+    <div className={styles.loader}>
       <TailSpin color="#00BFFF" height={80} width={80} />
     </div>
   );
 };
 
 export default Loader;
-
-
