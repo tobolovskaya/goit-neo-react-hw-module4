ef44f0bb2e7b0651e24728dc3ce042e95959a797
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-09-27 10:13:45 +0200

    Update ImageModal.jsx

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactModal from 'react-modal';
 import styles from './ImageModal.module.css';
 
-const ImageModal = ({ isOpen, onClose, image }) => {
+const ImageModal = ({ image, isOpen, onClose }) => {
   return (
-    <ReactModal isOpen={isOpen} onRequestClose={onClose} className={styles.modal}>
-      <img src={image.urls.regular} alt={image.alt_description} className={styles.modalImage} />
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className={styles.modal}
+      overlayClassName={styles.overlay}
+    >
+      <img src={image.urls.regular} alt={image.description} className={styles.image} />
+      <button onClick={onClose} className={styles.closeButton}>Close</button>
     </ReactModal>
   );
 };
