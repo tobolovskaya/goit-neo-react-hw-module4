306f51741804075d695e1790c4d7b1bcb90dc9ad
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-09-27 09:44:52 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ImageCard from '../ImageCard/ImageCard';
 import styles from './ImageGallery.module.css';
 
-const ImageGallery = ({ images }) => {
+const ImageGallery = ({ images, onImageClick }) => {
   return (
-    <ul className={styles.gallery}>
-      {images.map(image => (
-        <li key={image.id} className={styles.galleryItem}>
+    <ul className={styles.ImageGallery}>
+      {images.map((image) => (
+        <li key={image.id} onClick={() => onImageClick(image)}>
           <ImageCard image={image} />
         </li>
       ))}
