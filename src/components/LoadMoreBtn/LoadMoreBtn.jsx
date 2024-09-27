a44f58ee89d9f1ec64e7cce81a1cd9f668a89ba7
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-09-27 10:13:01 +0200

    Update LoadMoreBtn.jsx

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -3,11 +3,10 @@ import styles from './LoadMoreBtn.module.css';
 
 const LoadMoreBtn = ({ onClick }) => {
   return (
-    <button className={styles.LoadMoreBtn} onClick={onClick}>
+    <button className={styles.loadMoreBtn} onClick={onClick}>
       Load more
     </button>
   );
 };
 
 export default LoadMoreBtn;
-
